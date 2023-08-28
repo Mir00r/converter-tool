5a1471d6b48e12134d93b365240b25395313a786
Abdur Razzak <abdur.razzaq06@gmail.com>
2023-08-28 20:49:41 +0800

    converter enhancement is in progress for millions of data

--- src/main/java/com/sde/converter/exceltocsv/controllers/ExcelToCSVController.java
+++ src/main/java/com/sde/converter/exceltocsv/controllers/ExcelToCSVController.java
@@ -2,6 +2,7 @@ package com.sde.converter.exceltocsv.controllers;
 
 import com.sde.converter.commons.Constants;
 import com.sde.converter.exceltocsv.services.ExcelToCSVService;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
@@ -90,7 +91,8 @@ public class ExcelToCSVController {
     ) {
         try (InputStream excelInputStream = file.getInputStream()) {
 
-            byte[] zipContent = this.excelToCSVService.convertExcelToCsv(excelInputStream, separator);
+            byte[] zipContent = this.excelToCSVService.convertExcelToCsvZip(excelInputStream, separator, batchSize);
+//            byte[] zipContent = this.excelToCSVService.convertExcelToCsv(excelInputStream, separator, batchSize);
 
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.parseMediaType("application/zip"));
@@ -100,6 +102,28 @@ public class ExcelToCSVController {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(("Error converting file: " + Arrays.toString(e.getMessage().getBytes())).getBytes());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
+
+
+    @PostMapping("/excel-to-csv-zip1")
+    public ResponseEntity<Resource> convertExcelToCSV(@RequestParam("file") MultipartFile file) {
+        try (InputStream excelInputStream = file.getInputStream()) {
+            ByteArrayResource zipResource = this.excelToCSVService.convertExcelToCSV(excelInputStream);
+
+            // Set appropriate headers for downloading the ZIP file
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"output.zip\"");
+
+            return ResponseEntity.ok().headers(headers).body(zipResource);
+        } catch (IOException | InvalidFormatException e) {
+            e.printStackTrace();
+            // Handle error response
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+
 }
--- src/main/java/com/sde/converter/exceltocsv/services/ExcelToCSVService.java
+++ src/main/java/com/sde/converter/exceltocsv/services/ExcelToCSVService.java
@@ -1,14 +1,30 @@
 package com.sde.converter.exceltocsv.services;
 
+import com.opencsv.CSVWriter;
 import com.sde.converter.commons.Constants;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.eventusermodel.XSSFReader;
+import org.apache.poi.xssf.eventusermodel.XSSFSheetXMLHandler;
+import org.apache.poi.xssf.model.SharedStringsTable;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.xml.sax.*;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.helpers.XMLReaderFactory;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -68,11 +84,11 @@ public class ExcelToCSVService {
         workbook.close();
     }
 
-    @Async
+//    @Async
     public byte[] convertExcelToCsvZip(InputStream excelInputStream, String separator, int dataSize) throws IOException {
-//        Workbook workbook = new XSSFWorkbook(excelInputStream);
+        Workbook workbook = new XSSFWorkbook(excelInputStream);
 //        SXSSFWorkbook workbook = new SXSSFWorkbook(new XSSFWorkbook(excelInputStream));
-        Workbook workbook = WorkbookFactory.create(excelInputStream);
+//        Workbook workbook = WorkbookFactory.create(excelInputStream);
 
         ByteArrayOutputStream zipStream = new ByteArrayOutputStream();
         int batchSize = dataSize == 0 ? Constants.DEFAULT_BATCH_SIZE : dataSize;
@@ -92,6 +108,10 @@ public class ExcelToCSVService {
                             }
                         }
                         csvWriter.newLine();
+                    }
+                    // Dispose of resources
+                    if (workbook instanceof SXSSFWorkbook) {
+                        ((SXSSFWorkbook) workbook).dispose();
                     }
                 }
 
@@ -137,16 +157,24 @@ public class ExcelToCSVService {
         }
     }
 
-    public byte[] convertExcelToCsv(InputStream excelInputStream, String separator) throws IOException {
+    public byte[] convertExcelToCsv(InputStream excelInputStream, String separator, int dataSize) throws IOException {
         ByteArrayOutputStream zipStream = new ByteArrayOutputStream();
         try (ZipOutputStream zipOut = new ZipOutputStream(zipStream)) {
+            // Open the Excel file using SXSSFWorkbook
+//            Workbook workbook = new SXSSFWorkbook(new XSSFWorkbook(excelInputStream), 100); // 100 rows in memory at a time
+//            Sheet sheet1 = workbook.getSheetAt(0); // Get the desired sheet
+
+//            int totalNumberOfSheets = WorkbookFactory.create(excelInputStream).getNumberOfSheets();
+//            int totalNumberOfSheets = new XSSFWorkbook(excelInputStream).getNumberOfSheets();
+//            try (Workbook workbook = new SXSSFWorkbook(new XSSFWorkbook(excelInputStream), 100)) {
             try (Workbook workbook = WorkbookFactory.create(excelInputStream)) {
+                int totalNumberOfSheets = workbook.getNumberOfSheets();
                 // Iterate over sheets
-                for (int sheetIndex = 0; sheetIndex < workbook.getNumberOfSheets(); sheetIndex++) {
+                for (int sheetIndex = 0; sheetIndex < totalNumberOfSheets; sheetIndex++) {
                     Sheet sheet = workbook.getSheetAt(sheetIndex);
 
                     // Process sheet in chunks
-                    int batchSize = 1000; // Adjust as needed
+                    int batchSize = dataSize == 0 ? Constants.DEFAULT_BATCH_SIZE : dataSize; // Adjust as needed
                     for (int rowIndex = 0; rowIndex < sheet.getPhysicalNumberOfRows(); rowIndex += batchSize) {
                         int endIndex = Math.min(rowIndex + batchSize, sheet.getPhysicalNumberOfRows());
                         processAndWriteSheetData(sheet, rowIndex, endIndex, separator, zipOut);
@@ -177,6 +205,75 @@ public class ExcelToCSVService {
                 csvOutputStream.close();
             }
             csvWriter.flush();
+        }
+    }
+
+    public ByteArrayResource convertExcelToCSV(InputStream excelInputStream) throws IOException, InvalidFormatException {
+        File outputDirectory = createOutputDirectory(); // Create a temporary directory
+
+        OPCPackage opcPackage = OPCPackage.open(excelInputStream);
+        XSSFWorkbook workbook = new XSSFWorkbook(opcPackage);
+
+//        Workbook workbook = new XSSFWorkbook(opcPackage);
+
+        for (Sheet sheet : workbook) {
+            String csvFileName = sheet.getSheetName() + ".csv";
+            File csvFile = new File(outputDirectory, csvFileName);
+            try (CSVWriter csvWriter = new CSVWriter(new FileWriter(csvFile))) {
+                for (Row row : sheet) {
+                    String[] csvRow = new String[row.getLastCellNum()];
+
+                    for (int i = 0; i < row.getLastCellNum(); i++) {
+                        Cell cell = row.getCell(i, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+                        csvRow[i] = getCellValueAsString(cell);
+                    }
+                    csvWriter.writeNext(csvRow);
+                }
+            }
+        }
+        workbook.close();
+        opcPackage.close();
+
+        // Compress the generated CSV files into a single ZIP resource
+        ByteArrayOutputStream zipOutputStream = new ByteArrayOutputStream();
+        try (ZipOutputStream zipFile = new ZipOutputStream(zipOutputStream)) {
+            File[] csvFiles = outputDirectory.listFiles((dir, name) -> name.endsWith(".csv"));
+            for (File csvFile : csvFiles) {
+                try (FileInputStream csvInputStream = new FileInputStream(csvFile)) {
+                    ZipEntry zipEntry = new ZipEntry(csvFile.getName());
+                    zipFile.putNextEntry(zipEntry);
+                    byte[] buffer = new byte[1024];
+                    int len;
+                    while ((len = csvInputStream.read(buffer)) > 0) {
+                        zipFile.write(buffer, 0, len);
+                    }
+                    zipFile.closeEntry();
+                }
+            }
+        }
+        return new ByteArrayResource(zipOutputStream.toByteArray());
+    }
+
+    private File createOutputDirectory() {
+        File outputDirectory = new File("output");
+        if (!outputDirectory.exists()) {
+            outputDirectory.mkdirs();
+        }
+        return outputDirectory;
+    }
+
+    private String getCellValueAsString(Cell cell) {
+        switch (cell.getCellType()) {
+            case STRING:
+                return cell.getStringCellValue();
+            case NUMERIC:
+                return String.valueOf(cell.getNumericCellValue());
+            case BOOLEAN:
+                return String.valueOf(cell.getBooleanCellValue());
+            case FORMULA:
+                return cell.getCellFormula();
+            default:
+                return "";
         }
     }
 }
